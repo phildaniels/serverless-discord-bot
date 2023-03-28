c40efbcca9066b397d2c7b2d0153692cd2205b2c
Phil Daniels <05879@global.ul.com>
2023-03-27 18:09:48 -0600

    close to done figuring out docker

--- DiscordBot/index.ts
+++ DiscordBot/index.ts
@@ -1,24 +1,11 @@
 import { AzureFunctionServer, SlashCreator } from 'slash-create';
 import HelloCommand from './commands/hello.command';
 import ModalCommand from './commands/modal.command';
-import axios from 'axios';
 
 const creator = new SlashCreator({
   applicationID: process.env.DISCORD_APP_ID,
   publicKey: process.env.DISCORD_PUBLIC_KEY,
   token: process.env.DISCORD_BOT_TOKEN,
-});
-
-const server = new AzureFunctionServer(module.exports);
-server.addMiddleware((req, res, next) => {
-  axios
-    .post(`${process.env.WEBSITE_HOSTNAME}/api/echo`, req.body)
-    .then((response) => {
-      console.log('successful');
-    })
-    .catch((error) => {
-      console.log(error);
-    });
 });
 
 creator
